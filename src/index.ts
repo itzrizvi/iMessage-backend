41eee53d3d9727f2bece195c43f6f5dfa82153d0
itzrizvi <shahriar.me.work@gmail.com>
2023-07-20 02:49:32 +0600

    GraphQL Subscription Added

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,99 @@
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
-import pkg from 'body-parser';
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import pkg from "body-parser";
 const { json } = pkg;
-import typeDefs from './graphql/typeDefs';
-import resolvers from './graphql/resolvers';
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import dotenv from 'dotenv';
-import { getServerSession } from './utils/getServerSession';
-import { GraphQLContext, Session } from './utils/types';
-import { PrismaClient } from '@prisma/client';
+import typeDefs from "./graphql/typeDefs";
+import resolvers from "./graphql/resolvers";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import dotenv from "dotenv";
+import { getServerSession } from "./utils/getServerSession";
+import { GraphQLContext, Session, SubscriptionContext } from "./utils/types";
+import { PrismaClient } from "@prisma/client";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
+import { PubSub } from "graphql-subscriptions";
 dotenv.config();
 
 interface MyContext {
-    // token: String;
+  // token: String;
 }
 
 const corsOptions = {
-    origin: [`${process.env.CLIENT_ORIGIN}`],
-    credentials: true,
+  origin: [`${process.env.CLIENT_ORIGIN}`],
+  credentials: true,
 };
 // Context Params
 const prisma = new PrismaClient();
 // Pubsub
+const pubsub = new PubSub();
 
 const app = express();
 const schema = makeExecutableSchema({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
+
 const httpServer = http.createServer(app);
-const server = new ApolloServer<MyContext>({
+
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: "/graphql/subscriptions",
+});
+
+// WebSocketServer start listening.
+const serverCleanup = useServer(
+  {
     schema,
-    csrfPrevention: true,
-    cache: 'bounded',
-    plugins: [
-        ApolloServerPluginDrainHttpServer({ httpServer }),
-        ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-    ],
+    context: (ctx: SubscriptionContext): Promise<GraphQLContext> => {
+      if (ctx.connectionParams && ctx.connectionParams.session) {
+        const { session } = ctx.connectionParams;
+        return Promise.resolve({ session, prisma, pubsub });
+      }
+      return Promise.resolve({ session: null, prisma, pubsub });
+    },
+  },
+  wsServer,
+);
+
+const server = new ApolloServer<MyContext>({
+  schema,
+  csrfPrevention: true,
+  cache: "bounded",
+  plugins: [
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
+    },
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+  ],
 });
 await server.start();
 app.use(
-    '/graphql',
-    cors<cors.CorsRequest>(corsOptions),
-    json(),
-    expressMiddleware(server, {
-        context: async ({ req, res }): Promise<GraphQLContext> => {
-            const session = (await getServerSession(
-                req.headers.cookie,
-            )) as Session | null;
-            return { session, prisma };
-        },
-    }),
+  "/graphql",
+  cors<cors.CorsRequest>(corsOptions),
+  json(),
+  expressMiddleware(server, {
+    context: async ({ req, res }): Promise<GraphQLContext> => {
+      const session = (await getServerSession(
+        req.headers.cookie,
+      )) as Session | null;
+      return { session, prisma, pubsub };
+    },
+  }),
 );
 
 await new Promise<void>((resolve) =>
-    httpServer.listen({ port: 4000 }, resolve),
+  httpServer.listen({ port: 4000 }, resolve),
 );
 console.log(`🚀 Server ready at http://localhost:4000/graphql`);
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,41 +1,52 @@
-import { ISODateString } from 'next-auth';
-import { Prisma, PrismaClient } from '@prisma/client';
+import { ISODateString } from "next-auth";
+import { Prisma, PrismaClient } from "@prisma/client";
 import {
-    conversationPopulated,
-    participantPopulated,
-} from '../graphql/resolvers/conversations';
+  conversationPopulated,
+  participantPopulated,
+} from "../graphql/resolvers/conversations";
+import { Context } from "graphql-ws/lib/server";
+import { PubSub } from "graphql-subscriptions";
+
+// Server Config
 
 export interface GraphQLContext {
-    session: Session;
-    prisma: PrismaClient;
-    // pubsub
+  session: Session;
+  prisma: PrismaClient;
+  pubsub: PubSub;
 }
 
 // User Interface
 export interface Session {
-    user: User;
-    expires: ISODateString;
+  user: User;
+  expires: ISODateString;
+}
+
+// Subscription Context
+export interface SubscriptionContext extends Context {
+  connectionParams: {
+    session?: Session;
+  };
 }
 
 export interface User {
-    id: string;
-    username: string;
-    image: string;
-    email: string;
-    emailVerified: Boolean;
-    name: string;
+  id: string;
+  username: string;
+  image: string;
+  email: string;
+  emailVerified: Boolean;
+  name: string;
 }
 
 export interface CreateUsernameResponse {
-    success?: boolean;
-    error?: string;
+  success?: boolean;
+  error?: string;
 }
 
 // Converations
 export type ConversationPopulated = Prisma.ConversationGetPayload<{
-    include: typeof conversationPopulated;
+  include: typeof conversationPopulated;
 }>;
 
 export type ParticipantPopulated = Prisma.ConversationParticipantGetPayload<{
-    include: typeof participantPopulated;
+  include: typeof participantPopulated;
 }>;
