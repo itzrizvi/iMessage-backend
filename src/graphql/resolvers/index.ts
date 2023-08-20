001a0090e25f73a37273030c43e517584350df76
itzrizvi <shahriar.me.work@gmail.com>
2023-08-20 22:36:52 +0600

    Date Sacalar resolver added

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,6 +1,7 @@
 import userResolvers from "./user";
 import conversationResolvers from "./conversations";
 import messageResolvers from "./message";
+import scalarsResolvers from "./scalars";
 import merge from "lodash.merge";
 
 const resolvers = merge(
@@ -8,6 +9,7 @@ const resolvers = merge(
   userResolvers,
   conversationResolvers,
   messageResolvers,
+  scalarsResolvers,
 );
 
 export default resolvers;
